e59fb01c34ce43016662aa9bafc3847379873f9b
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-09-20 18:40:35 -0500

    updating imdb tables

--- lab1/imdb_tables.sql
+++ lab1/imdb_tables.sql
@@ -8,3 +8,28 @@ WITH SERDEPROPERTIES (
   'field.delim' = ','
 ) LOCATION 's3://cs327e-fall2017-imdb/athena/directors/'
 TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.person_basics (
+  `person_id` string,
+  `primary_name` string,
+  `birth_year` int,
+  `death_year` int 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/person_basics/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
+CREATE EXTERNAL TABLE IF NOT EXISTS imdb.person_professions (
+  `person_id` string,
+  `profession` string 
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+) LOCATION 's3://cs327e-fall2017-imdb/athena/person_professions/'
+TBLPROPERTIES ('has_encrypted_data'='false');
+
