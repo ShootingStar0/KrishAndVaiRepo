9b614ff4f63bc40daa667df82fd400e6ee21d1f5
Vaidehi Narayan <vaidehi.narayan@utexas.edu>
2017-09-21 20:09:32 -0500

    queries 1-10

--- lab1/imdb_queries.sql
+++ lab1/imdb_queries.sql
@@ -37,3 +37,47 @@ from person_basics pb join person_professions pp
 on pb.person_id = pp.person_id
 order by pb.person_id;
 
+
+
+/* 6. listing out the names of people who are both in the directors and in the writers tables (they're both directors and writers), without repeats note: this one takes a long time to run so I tested it with a limit!*/
+
+select distinct pb.primary_name, pb.person_id
+from person_basics pb join writers w
+on pb.person_id = w.person_id
+join directors d on pb.person_id = d.person_id;
+
+
+
+/* 7. listing the title names with their season number while excluding adult titles and titles without season numbers, and ordering by the title ids*/
+
+select tb.title_id, tb.primary_title, te.parent_title, te.season_num
+from title_basics tb join title_episodes te 
+on tb.title_id = te.title_id 
+where tb.is_adult = False and te.season_num is not null
+order by tb.title_id;
+
+
+/* 8. list titles and their respective genre with ratings higher than 9 and in descending order by year */
+
+select distinct tb.primary_title, tg.genre, tr.average_rating, tb.start_year
+from title_basics tb join title_genres tg on tb.title_id = tg.title_id
+join title_ratings tr on tb.title_id = tr.title_id
+where tr.average_rating > 9
+order by tb.start_year desc;
+
+
+/* 9. list titles and their ratings that have over a million votes, and order them by the number of votes*/ 
+
+select tb.primary_title, tr.average_rating, tr.num_votes
+from  title_basics tb join title_ratings tr 
+on tb.title_id = tr.title_id 
+where tr.num_votes > 1000000
+order by num_votes desc;
+
+
+/* 10. listing directors with their birth and death years, and including only directors who have a birth and a death year*/
+
+select distinct pb.primary_name, pb.birth_year, pb.death_year
+from person_basics pb join directors d 
+on pb.person_id = d.person_id 
+where death_year is not null and birth_year is not null;
